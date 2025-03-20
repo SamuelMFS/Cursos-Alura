874672cf5096d66d82297e989a8246bd02cd4c98
SamuelMFS <samuelzin1807@gmail.com>
2025-03-20 17:54:18 -0300

    aulas 1-6 react com typescript

--- react/alura-studies/src/Components/List/Item/index.tsx
+++ react/alura-studies/src/Components/List/Item/index.tsx
@@ -2,7 +2,6 @@ import style from '../List.module.scss';
 import { ITasks } from '../../../types/task';
 
 export default function Item({name, time, selected, completed, id}: ITasks) {
-  console.log('item atula:', {name, time, selected, completed, id})
   return(
     <li className={style.item}>
       <h3>
